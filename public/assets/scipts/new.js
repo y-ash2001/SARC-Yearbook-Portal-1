9321247cb9de6fc46744c54edbe6de498a71b55e
y-ash2001 <f20191026@pilani.bits-pilani.ac.in>
2020-06-24 01:41:00 +0530

    Added modal for photo upload

--- public/assets/scipts/new.js
+++ public/assets/scipts/new.js
@@ -1,12 +1,25 @@
 var modal = document.getElementById('modal-main');
 var mask = document.getElementById('modal-mask');
+var photo_modal = document.getElementById('photo-modal');
+var modal_mask = document.getElementById('upload-modal');
 // Get the <span> element that closes the modal
 var span = document.getElementById('close-modal');
-
+var close_button = document.getElementById('close-photo-modal');
+var photo_btn = document.getElementById('photo-button');
 // When the user clicks on <span> (x), close the modal
+photo_btn.onclick = function () {
+	modal_mask.style.display = 'block';
+	photo_modal.style.display = 'block';
+};
+
 span.onclick = function () {
 	modal.style.display = 'none';
 	mask.style.display = 'none';
+};
+
+close_button.onclick = function () {
+	modal_mask.style.display = 'none';
+	photo_modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
@@ -14,5 +27,12 @@ window.onclick = function (event) {
 	if (event.target == modal || event.target == mask) {
 		modal.style.display = 'none';
 		mask.style.display = 'none';
+	}
+};
+
+window.onclick = function (event) {
+	if (event.target == modal_mask || event.target == photo_modal) {
+		modal_mask.style.display = 'none';
+		photo_modal.style.display = 'none';
 	}
 };
